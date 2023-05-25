aaa1c936afb9d6766adff8ce92d9aa3c83cd15bc
remishevskaya <heyjude945@gmail.com>
2023-05-25 19:45:32 +0300

    Сделала задание 12

--- Seminar2/Task12.py
+++ Seminar2/Task12.py
@@ -1,6 +1,24 @@
-number_x =
+import math
+
+summ = 0
+comp = 0
+
 try:
-    coins = int(input('Введите количество монеток: '))
+    summ = int(input('Введите сумму чисел: '))
+    comp = int(input('Введите произведение чисел: '))
+
+    Diskr = summ ** 2 - 4 * comp
+
+    if Diskr < 0:
+        print('Таких чисел нет')
+    elif Diskr == 0:
+        x = summ // 2
+        print(f'Числа X и Y равны и составляют {x}')
+    elif Diskr > 0:
+        x = (summ + math.sqrt(Diskr)) // 2
+        y = (summ - math.sqrt(Diskr)) // 2
+        print(f'Число X: {x}  \n Число Y: {y}')
+
 except ValueError:
     print('Введенное количество недействительно')
     exit()
