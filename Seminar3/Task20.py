c7b3a397c347bbc0275fc3311bc94a65a54519b3
remishevskaya <heyjude945@gmail.com>
2023-05-28 22:19:05 +0300

    Доделала 20 задание

--- Seminar3/Task20.py
+++ Seminar3/Task20.py
@@ -1,23 +1,22 @@
-try:
-    score_dict = {1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'],
-                  2: ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'],
-                  3: ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'],
-                  4: ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'],
-                  5: ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч'],
-                  8: ['J', 'X', 'Ш', 'Э', 'Ю'],
-                  10: ['Q', 'Z', 'Ф', 'Щ', 'Ъ']}
+score_dict = {1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т'],
+              2: ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У'],
+              3: ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я'],
+              4: ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы'],
+              5: ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч'],
+              8: ['J', 'X', 'Ш', 'Э', 'Ю'],
+              10: ['Q', 'Z', 'Ф', 'Щ', 'Ъ']}
 
-    world = input('Введите слово: ')
-    score = 0
-
-    for i in world:
-        for key, value in score_dict.items():
-            for letter in value:
-                if letter == i.upper():
-                    score = score + key
-
-    print(f'Количество очков: {score}')
-
-except ValueError:
-    print('Введенное значение недействительно')
+world = input('Введите слово: ')
+if world.isdigit():
+    print('Вы ввели не слово')
     exit()
+
+score = 0
+
+for i in world:
+    for key, value in score_dict.items():
+        for letter in value:
+            if letter == i.upper():
+                score = score + key
+
+print(f'Количество очков: {score}')
